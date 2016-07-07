602a9baff9f59a5eab79a37e9ac168e26c768bee
Javier Zapata <javier.zapata333@gmail.com>
2016-07-07 23:23:30 +0000

    lodown changes

--- index.js
+++ index.js
@@ -3,10 +3,11 @@
 'use strict';
 
 const customers = require("./data/customers.json");
-
+const my_library = require("./node_modules/jzapata333");
 
 /**
  * 1. Import your lodown module using the require() method
  * 2. Solve all problems as outlined in the README.
  */
  
+console.log(my_library);
